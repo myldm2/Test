017a5c045c6c70d7c79f2f9233b7cfe7dca82460
mayinglun <mayinglun@163.com>
2018-11-29 17:18:45 +0800

    提交

--- PlayerTest/MAVedioPlayer/OpenGLView.m
+++ PlayerTest/MAVedioPlayer/OpenGLView.m
@@ -8,6 +8,39 @@
 
 #import "OpenGLView.h"
 
+#define FSH @"varying lowp vec2 TexCoordOut;\
+\
+uniform sampler2D SamplerY;\
+uniform sampler2D SamplerU;\
+uniform sampler2D SamplerV;\
+\
+void main(void)\
+{\
+mediump vec3 yuv;\
+lowp vec3 rgb;\
+\
+yuv.x = texture2D(SamplerY, TexCoordOut).r;\
+yuv.y = texture2D(SamplerU, TexCoordOut).r - 0.5;\
+yuv.z = texture2D(SamplerV, TexCoordOut).r - 0.5;\
+\
+rgb = mat3( 1,       1,         1,\
+0,       -0.39465,  2.03211,\
+1.13983, -0.58060,  0) * yuv;\
+\
+gl_FragColor = vec4(rgb, 1);\
+\
+}"
+
+#define VSH @"attribute vec4 position;\
+attribute vec2 TexCoordIn;\
+varying vec2 TexCoordOut;\
+\
+void main(void)\
+{\
+gl_Position = position;\
+TexCoordOut = TexCoordIn;\
+}"
+
 enum AttribEnum
 {
     ATTRIB_VERTEX,
@@ -24,18 +57,256 @@ enum TextureType
     TEXC
 };
 
+@interface OpenGLView ()
+{
+    /**
+     OpenGL绘图上下文
+     */
+    EAGLContext             *_glContext;
+    
+    /**
+     帧缓冲区
+     */
+    GLuint                  _framebuffer;
+    
+    /**
+     渲染缓冲区
+     */
+    GLuint                  _renderBuffer;
+    
+    /**
+     着色器句柄
+     */
+    GLuint                  _program;
+    
+    /**
+     YUV纹理数组
+     */
+    GLuint                  _textureYUV[3];
+    
+    /**
+     视频宽度
+     */
+    GLuint                  _videoW;
+    
+    /**
+     视频高度
+     */
+    GLuint                  _videoH;
+    
+    GLsizei                 _viewScale;
+    
+    //void                    *_pYuvData;
+    
+}
+
+@end
+
 @implementation OpenGLView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        if (![self setup]) {
+            self = nil;
+        }
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        if (![self setup]) {
+            self = nil;
+        }
+    }
+    return self;
+}
 
 + (Class)layerClass
 {
     return [CAEAGLLayer class];
 }
 
-- (BOOL)setup
+- (CAEAGLLayer *)eaglLayer
 {
-    return true;
+    return (CAEAGLLayer*) self.layer;
 }
 
+- (BOOL)setup
+{
+    CAEAGLLayer * eaglLayer = [self eaglLayer];
+    eaglLayer.opaque = YES;
+    
+    eaglLayer.drawableProperties = @{
+                                     kEAGLDrawablePropertyRetainedBacking: @NO,
+                                     kEAGLDrawablePropertyColorFormat: kEAGLColorFormatRGB565,
+                                     };
+    self.contentScaleFactor = [UIScreen mainScreen].scale;
+    _viewScale = [UIScreen mainScreen].scale;
+    
+    _glContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    if (!_glContext || ![EAGLContext setCurrentContext:_glContext])
+    {
+        return NO;
+    }
+    
+    [self setupYUVTexture];
+    
+    [self loadShader];
+    
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    
+    glUseProgram(_program);
+    
+    GLuint textureUniformY = glGetUniformLocation(_program, "SamplerY");
+    GLuint textureUniformU = glGetUniformLocation(_program, "SamplerU");
+    GLuint textureUniformV = glGetUniformLocation(_program, "SamplerV");
+    
+    glUniform1i(textureUniformY, 0);
+    glUniform1i(textureUniformU, 1);
+    glUniform1i(textureUniformV, 2);
+    
+    return YES;
+}
 
+- (void)setupYUVTexture
+{
+    if (_textureYUV[TEXY])
+    {
+        glDeleteTextures(3, _textureYUV);
+    }
+    
+    glGenTextures(3, _textureYUV);
+    if (!_textureYUV[TEXY] || !_textureYUV[TEXU] || !_textureYUV[TEXV])
+    {
+        return;
+    }
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, _textureYUV[TEXY]);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, _textureYUV[TEXU]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, _textureYUV[TEXV]);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+}
+
+- (void)loadShader
+{
+    GLuint vertexShader = [self compileShader:VSH withType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:FSH withType:GL_FRAGMENT_SHADER];
+    _program = glCreateProgram();
+    glAttachShader(_program, vertexShader);
+    glAttachShader(_program, fragmentShader);
+    
+    glBindAttribLocation(_program, ATTRIB_VERTEX, "position");
+    glBindAttribLocation(_program, ATTRIB_TEXTURE, "TexCoordIn");
+    
+    glLinkProgram(_program);
+    
+    GLint linkSuccess;
+    glGetProgramiv(_program, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE)
+    {
+        GLchar messages[256];
+        glGetProgramInfoLog(_program, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"<<<<着色器连接失败 %@>>>", messageString);
+        return;
+    }
+    
+    if (vertexShader)
+    {
+        glDeleteShader(vertexShader);
+    }
+    if (fragmentShader) {
+        glDeleteShader(fragmentShader);
+    }
+}
+
+- (GLuint)compileShader:(NSString*)shaderString withType:(GLenum)shaderType
+{
+    if (!shaderString)
+    {
+        return 0;
+    }
+    
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    const char* shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = (int)[shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    glCompileShader(shaderHandle);
+    
+    GLint compileSuccess;
+    
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE)
+    {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
+        NSString* messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        return 0;
+    }
+    
+    return shaderHandle;
+}
+
+- (BOOL)createFrameAndRanderBuffer
+{
+    glGenFramebuffers(1, &_framebuffer);
+    glGenRenderbuffers(1, &_renderBuffer);
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _renderBuffer);
+    
+    if (![_glContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:[self eaglLayer]])
+    {
+        NSLog(@"attach渲染缓冲区失败");
+    }
+    
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _renderBuffer);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        NSLog(@"创建缓冲区错误 0x%x", glCheckFramebufferStatus(GL_FRAMEBUFFER));
+        return NO;
+    }
+    return YES;
+}
+
+- (void)destoryFrameAndRanderBuffer
+{
+    if (_framebuffer)
+    {
+        glDeleteFramebuffers(1, &_framebuffer);
+    }
+    if (_renderBuffer)
+    {
+        glDeleteRenderbuffers(1, &_renderBuffer);
+    }
+    _framebuffer = 0;
+    _renderBuffer = 0;
+}
 
 @end
