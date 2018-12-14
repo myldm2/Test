438a1acd81a68dd52ac437d937975fa927409c11
mayinglun <mayinglun@163.com>
2018-12-14 17:40:56 +0800

    提交代码；

--- PlayerTest/MAVedioPlayer/FFmpegDecoder.m
+++ PlayerTest/MAVedioPlayer/FFmpegDecoder.m
@@ -55,10 +55,41 @@
     return r.num == 0 || r.den == 0 ? 0.0 : (double)r.num/(double)r.den;
 }
 
-- (BOOL)OpenUrl:(const char *)path
-{
-    [self Close];
-    
-}
+//- (BOOL)OpenUrl:(const char *)path
+//{
+//    [self Close];
+//    [lock lock];
+//    int re = avformat_open_input(&pFormatCtx, path, 0, 0);
+//    if (re != 0)
+//    {
+//        [lock unlock];
+//        av_strerror(re, errorBuf, sizeof(errorBuf));
+//        return false;
+//    }
+//    _totalMs = (int)(pFormatCtx->duration/AV_TIME_BASE)*1000;
+//    avformat_find_stream_info(pFormatCtx, NULL);
+//    
+//    for (int i = 0; i < pFormatCtx->nb_streams; i ++) {
+//        AVStream* stream = pFormatCtx->streams[i];
+//        AVCodec* codec = avcodec_find_decoder(stream->codecpar->codec_id);
+//        AVCodecContext* codecCtx = avcodec_alloc_context3(codec);
+//        avcodec_parameters_to_context(codecCtx, stream->codecpar);
+//        
+//        if (codecCtx->codec_type == AVMEDIA_TYPE_VIDEO)
+//        {
+//            printf("video\n");
+//            _videoStreamIndex = i;
+//            pVideoCodec = codec;
+//            pVideoCodecCtx = codecCtx;
+//            int err = avcodec_open2(pVideoCodecCtx, pVideoCodec, NULL);
+//            if (err != 0)
+//            {
+//                [lock unlock];
+//                
+//            }
+//        }
+//        
+//    }
+//}
 
 @end
